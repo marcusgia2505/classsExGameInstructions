f22b813cf5d776f33b7bb71ad63523bc83de990d
Marcus Giamattei <marcus.giamattei@uni-passau.de>
2018-10-18 12:33:50 +0200

    logos added

--- docs/conf.py
+++ docs/conf.py
@@ -119,12 +119,12 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/pic/logo.png'
 
 # The name of an image file (within the _static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/pic/favicon.ico'
 
 # Add any paths that contain custom _static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin _static files,
