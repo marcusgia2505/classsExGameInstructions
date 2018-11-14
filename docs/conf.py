3d0f0475a3caca7dc12a4b9ad436d7f300f02c25
Marcus Giamattei <marcus.giamattei@uni-passau.de>
2018-11-14 16:01:34 +0100

    codehighlight

--- docs/conf.py
+++ docs/conf.py
@@ -101,8 +101,9 @@ from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
 
 # enable highlighting for PHP code not between <?php ... ?> by default
-lexers['php'] = PhpLexer(startinline=True)
-lexers['php-annotations'] = PhpLexer(startinline=True)
+lexers["php"] = PhpLexer(startinline=True, linenos=1)
+lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
+primary_domain = "php"
 
 
 # -- Options for HTML output ----------------------------------------------
